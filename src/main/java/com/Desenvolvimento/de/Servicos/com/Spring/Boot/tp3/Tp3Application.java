885f03efb005db5a1b0792266d4d0b48360cf864
brennomachadoeng-max <brennomachadoeng@gmail.com>
2026-06-07 16:58:25 -0300

    .

--- src/main/java/com/Desenvolvimento/de/Servicos/com/Spring/Boot/tp3/Tp3Application.java
+++ src/main/java/com/Desenvolvimento/de/Servicos/com/Spring/Boot/tp3/Tp3Application.java
@@ -2,8 +2,10 @@ package com.Desenvolvimento.de.Servicos.com.Spring.Boot.tp3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Tp3Application {
 
 	public static void main(String[] args) {
